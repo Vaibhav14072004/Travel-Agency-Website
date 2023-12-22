b763b90ba28cfc463b29f07eadccc00e8ed97f90
Vaibhav <129673206+Vaibhav14072004@users.noreply.github.com>
2023-12-22 22:28:56 +0530

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'restaurentms';
+
+// Create connection
+$sql = mysqli_connect($host, $username, $password, $database);
+
+// Check connection
+// if (!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// } else {
+//     echo "Connected successfully";
+// }
+
+// Rest of your code goes here...
+
+?>
